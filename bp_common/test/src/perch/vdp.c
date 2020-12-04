29fb3b653e213171e164e0bc3102925501fc954b
Zahra Azad <zazad@bu.edu>
2020-12-03 19:40:44 -0500

    fix dma out

--- bp_common/test/src/perch/vdp.c
+++ bp_common/test/src/perch/vdp.c
@@ -109,8 +109,51 @@ void bp_call_vector_add_accelerator(uint8_t type, struct VDP_CSR vdp_csrs){
   }
 }
 
+void bp_hw_dma(uint64_t *cfg_base_dma_addr, uint64_t *src, uint64_t length)
+{
+  bp_set_mmio_csr(cfg_base_dma_addr, DATA_PTR, (uint64_t) src);
+  bp_set_mmio_csr(cfg_base_dma_addr, DATA_LEN, length);
+  bp_set_mmio_csr(cfg_base_dma_addr, START_DMA, 1);
+  uint64_t status;
+  while (1)
+    {
+      status = bp_get_mmio_csr(cfg_base_dma_addr, DONE_DMA);
+      if(status)
+        break;
+    }
 
-void bp_call_zipline_accelerator(uint8_t type, struct VDP_CSR vdp_csrs, struct zipline_tlv *tlv_headers, uint64_t tlv_num)
+}
+void bp_call_zipline_accelerator(uint8_t type, struct Zipline_CSR zipline_csrs, uint64_t input_tlv_num)
+{
+  uint64_t *sac_cfg = (uint64_t *) 0x0020000a;
+  bp_set_mmio_csr(sac_cfg, 0, 1);//enable sac mem region csr
+ 
+  uint64_t *cfg_base_addr;
+  uint64_t *cfg_base_dma_addr;
+
+  cfg_base_addr = (uint64_t *)(0x02100000);//device 1/compression engine in the tile
+  cfg_base_dma_addr = (uint64_t *)(0x02200000);//dma device id
+  bp_set_mmio_csr(cfg_base_addr, RESP_PTR, (uint64_t) zipline_csrs.resp_ptr);
+  //send input
+  int i=0;
+  for (i=0; i < input_tlv_num; i++)
+  {
+    bp_set_mmio_csr(cfg_base_addr, TLV_TYPE, zipline_csrs.input_ptr[i].type);
+    bp_hw_dma(cfg_base_dma_addr, zipline_csrs.input_ptr[i].data_ptr, zipline_csrs.input_ptr[i].length);
+  }
+
+  //receive output
+  uint64_t status;
+  while (1)
+    {
+      status = bp_get_mmio_csr(cfg_base_addr, RESP_DONE);
+      if(status)
+        break;
+    }
+  
+}
+
+/*void bp_call_zipline_accelerator(uint8_t type, struct VDP_CSR vdp_csrs, struct zipline_tlv *tlv_headers, uint64_t tlv_num)
 {
   uint64_t *cfg_base_addr;
   uint64_t csr_value;
@@ -118,13 +161,6 @@ void bp_call_zipline_accelerator(uint8_t type, struct VDP_CSR vdp_csrs, struct z
 
   uint64_t *sac_cfg = (uint64_t *) 0x0020000a;
   bp_set_mmio_csr(sac_cfg, 0, 1);//enable sac mem region csr
-  /*int i=0;
-  uint64_t test = tlv_headers[0].tlv_header;
-  for(i = 0; i < 16;++i){
-    bp_cprint(TO_HEX((uint8_t)((test>>i*4) & 0x0F)));
-    }*/
-
-  //cfg_base_addr = type ? SACCEL_VDP_BASE_ADDR : CACCEL_VADD_BASE_ADDR;
   cfg_base_addr = (uint64_t *)(0x02100000);//chnage the device id to dma in the tile
   uint64_t tlv_count =0;
   int i=0;
@@ -156,3 +192,4 @@ void bp_call_zipline_accelerator(uint8_t type, struct VDP_CSR vdp_csrs, struct z
   dma_cpy((uint64_t *) &csr_value, vdp_csrs.resp_ptr, 1);
 
 }
+*/
--- bp_common/test/src/perch/vdp.h
+++ bp_common/test/src/perch/vdp.h
@@ -30,19 +30,34 @@ struct VDP_CSR
   uint64_t *tlv_header_ptr;
 };
 
+//Zipline CSR IDX
 #define TLV_TYPE       0 
-#define TLV_IDX        1
-struct zipline_tlv
+#define RESP_PTR       1
+#define RESP_DONE      2
+//HW DMA CSR IDX 
+#define DATA_PTR       0
+#define DATA_LEN       1
+#define START_DMA      2
+#define DONE_DMA       3
+
+
+struct dma_cfg 
 {
-  uint64_t tlv_header;//content of a header
-  uint64_t tlv_idx;//1:sot, 0:mot, 2:eot, 3:sot-eot
-  uint64_t header_type;//0:rqe, 1:cmd, 2:frmd, 3:dara, 4:cqe
+  uint64_t *data_ptr;
+  uint64_t length;
+  uint8_t  type;//0:req, 1:cmd, 2:frmd, 3:data, 4:cqe 
+};
+
+struct Zipline_CSR
+{
+  struct dma_cfg *input_ptr;
+  uint64_t *resp_ptr;
 };
 
 void bp_set_mmio_csr(uint64_t *accel_base_address, uint8_t csr_idx, uint64_t csr_value);
 uint64_t bp_get_mmio_csr(uint64_t *accel_base_address, uint8_t csr_idx); 
 void dma_cpy(uint64_t *src, uint64_t *dest, uint64_t length);
-
+void bp_hw_dma(uint64_t *cfg_base_dma_addr, uint64_t *src, uint64_t length);
 void bp_vdp_config_accelerator(uint64_t *base_cfg_addr, uint64_t *input_a_ptr,
                                uint64_t *input_b_ptr, uint64_t input_length, 
                                uint64_t operation, uint64_t *resp_ptr, uint64_t resp_length);
@@ -51,5 +66,5 @@ void bp_vdp_wait_for_completion(uint64_t *base_cfg_addr);
 void bp_call_vector_dot_product_accelerator(uint8_t type, struct VDP_CSR vdp_csrs);
 void bp_call_vector_add_accelerator(uint8_t type, struct VDP_CSR vdp_csrs);
 
-void bp_call_zipline_accelerator(uint8_t type, struct VDP_CSR vdp_csrs, struct zipline_tlv *tlv_headers, uint64_t tlv_num);
+void bp_call_zipline_accelerator(uint8_t type, struct Zipline_CSR vdp_csrs, uint64_t input_tlv_num);
 #endif
