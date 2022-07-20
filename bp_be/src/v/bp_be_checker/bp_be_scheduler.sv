wasabifan-bp-forks/black-parrot

commit 9c9c1a65657dfbb04ba458795e7f01b64ee71944

Fixing misaligned instruction handling (should be in backend) (#1100)
* Fixing misaligned instruction handling (should be in backend)

* Typo

bp_be/src/v/bp_be_checker/bp_be_scheduler.sv
@@ -212,8 +212,6 @@ module bp_be_scheduler
       dispatch_pkt.exception.instr_access_fault |=
         fe_exc_not_instr_li & fe_queue_lo.msg.exception.exception_code inside {e_instr_access_fault};
       dispatch_pkt.exception.instr_misaligned   |=
-        fe_exc_not_instr_li & fe_queue_lo.msg.exception.exception_code inside {e_instr_misaligned};
-      dispatch_pkt.exception.instr_page_fault   |=
         fe_exc_not_instr_li & fe_queue_lo.msg.exception.exception_code inside {e_instr_page_fault};
       dispatch_pkt.exception.itlb_miss          |=
         fe_exc_not_instr_li & fe_queue_lo.msg.exception.exception_code inside {e_itlb_miss};

bp_be/src/v/bp_be_calculator/bp_be_calculator_top.sv
@@ -31,9 +31,6 @@ module bp_be_calculator_top
    , localparam ptw_fill_pkt_width_lp   = `bp_be_ptw_fill_pkt_width(vaddr_width_p, paddr_width_p)
    , localparam wb_pkt_width_lp         = `bp_be_wb_pkt_width(vaddr_width_p)
    , localparam decode_info_width_lp    = `bp_be_decode_info_width
-
-   // From BP BE specifications
-   , localparam pipe_stage_els_lp = 5
    )
  (input                                             clk_i
   , input                                           reset_i
@@ -101,8 +98,9 @@ module bp_be_calculator_top
 
 
   // Pipeline stage registers
-  bp_be_exc_stage_s      [pipe_stage_els_lp  :0] exc_stage_n;
-  bp_be_exc_stage_s      [pipe_stage_els_lp-1:0] exc_stage_r;
+  localparam pipe_stage_els_lp = 5;
+  bp_be_exc_stage_s [pipe_stage_els_lp  :0] exc_stage_n;
+  bp_be_exc_stage_s [pipe_stage_els_lp-1:0] exc_stage_r;
 
   bp_be_wb_pkt_s [pipe_stage_els_lp  :0] comp_stage_n;
   bp_be_wb_pkt_s [pipe_stage_els_lp-1:0] comp_stage_r;
@@ -112,6 +110,8 @@ module bp_be_calculator_top
   rv64_frm_e           frm_dyn_lo;
 
   bp_be_wb_pkt_s long_iwb_pkt, long_fwb_pkt;
+
+  logic pipe_ctl_instr_misaligned_lo;
 
   logic pipe_mem_dtlb_store_miss_lo;
   logic pipe_mem_dtlb_load_miss_lo;
@@ -204,6 +204,7 @@ module bp_be_calculator_top
      ,.data_o(pipe_ctl_data_lo)
      ,.br_pkt_o(br_pkt_o)
      ,.v_o(pipe_ctl_data_lo_v)
+     ,.instr_misaligned_v_o(pipe_ctl_instr_misaligned_lo)
      );
 
   // Computation pipelines
@@ -477,6 +478,8 @@ module bp_be_calculator_top
 
           exc_stage_n[1].exc.illegal_instr      |= pipe_sys_illegal_instr_lo;
           exc_stage_n[1].spec.csrw              |= pipe_sys_csrw_lo;
+
+          exc_stage_n[1].exc.instr_misaligned   |= pipe_ctl_instr_misaligned_lo;
 
           exc_stage_n[1].exc.dtlb_store_miss    |= pipe_mem_dtlb_store_miss_lo;
           exc_stage_n[1].exc.dtlb_load_miss     |= pipe_mem_dtlb_load_miss_lo;

bp_be/src/v/bp_be_calculator/bp_be_pipe_ctl.sv
@@ -21,15 +21,16 @@ module bp_be_pipe_ctl
    , localparam dispatch_pkt_width_lp = `bp_be_dispatch_pkt_width(vaddr_width_p)
    , localparam branch_pkt_width_lp = `bp_be_branch_pkt_width(vaddr_width_p)
    )
-  (input                               clk_i
-   , input                             reset_i
+  (input                                    clk_i
+   , input                                  reset_i
 
-   , input [dispatch_pkt_width_lp-1:0] reservation_i
-   , input                             flush_i
+   , input [dispatch_pkt_width_lp-1:0]      reservation_i
+   , input                                  flush_i
 
-   , output [dpath_width_gp-1:0]        data_o
-   , output [branch_pkt_width_lp-1:0]  br_pkt_o
-   , output                            v_o
+   , output logic [dpath_width_gp-1:0]      data_o
+   , output logic [branch_pkt_width_lp-1:0] br_pkt_o
+   , output logic                           v_o
+   , output logic                           instr_misaligned_v_o
    );
 
   // Suppress unused signal warning
@@ -69,16 +70,18 @@ module bp_be_pipe_ctl
       end
 
   wire [vaddr_width_p-1:0] baddr = decode.baddr_sel ? rs1 : pc;
-  wire [vaddr_width_p-1:0] taken_tgt = baddr + imm;
+  wire [vaddr_width_p-1:0] taken_raw = baddr + imm;
+  wire [vaddr_width_p-1:0] taken_tgt = {taken_raw[vaddr_width_p-1:1], 1'b0};
   wire [vaddr_width_p-1:0] ntaken_tgt = pc + 4'd4;
 
   assign data_o   = vaddr_width_p'($signed(ntaken_tgt));
   assign v_o      = reservation.v & reservation.decode.pipe_ctl_v;
+  assign instr_misaligned_v_o = btaken & (taken_tgt[1:0] != 2'b00);
 
   assign br_pkt.v         = reservation.v & reservation.queue_v & ~flush_i;
   assign br_pkt.branch    = br_pkt.v & reservation.decode.pipe_ctl_v;
   assign br_pkt.btaken    = br_pkt.v & reservation.decode.pipe_ctl_v & btaken;
-  assign br_pkt.npc       = btaken ? {taken_tgt[vaddr_width_p-1:1], 1'b0} : ntaken_tgt;
+  assign br_pkt.npc       = btaken ? taken_tgt : ntaken_tgt;
 
 endmodule
 

bp_common/src/include/bp_common_core_pkgdef.svh
@@ -61,19 +61,18 @@
   } bp_fe_misprediction_reason_e;
 
   /*
-   * The exception code types. e_itlb_miss is when the instruction has a miss in
-   * the iTLB. ITLB misses can cause the instruction misaligned. Thus the frontend
-   * detects the instruction miss first and then detect whether there is an ITLB
-   * miss. e_instruction_access_fault is when the access control is violated.
-   * e_instr_page_fault is when the instruction page is accessed with insufficent privilege
+   * The exception code types.
+   * e_itlb_miss is for an ITLB miss
+   * e_instr_page_fault is for an access page fault
+   * e_instr_access_fault is when the physical address is not allowed for the access type
+   * e_icache_miss is for a nonspeculative I$ miss which needs to be confirmed by the backend
    */
   typedef enum logic [2:0]
   {
     e_itlb_miss           = 0
-    ,e_icache_miss        = 1
-    ,e_instr_misaligned   = 2
-    ,e_instr_access_fault = 3
-    ,e_instr_page_fault   = 4
+    ,e_instr_page_fault   = 1
+    ,e_instr_access_fault = 2
+    ,e_icache_miss        = 3
   } bp_fe_exception_code_e;
 
   /*

bp_fe/src/v/bp_fe_top.sv
@@ -210,7 +210,7 @@ module bp_fe_top
   assign attaboy_v_li               = attaboy_v;
   assign attaboy_pc_li              = fe_cmd_cast_i.vaddr;
 
-  logic instr_misaligned_v, instr_access_fault_v, instr_page_fault_v;
+  logic instr_access_fault_v, instr_page_fault_v;
   logic ptag_v_li, ptag_uncached_li, ptag_nonidem_li, ptag_dram_li, ptag_miss_li;
   logic [ptag_width_p-1:0] ptag_li;
 
@@ -259,7 +259,7 @@ module bp_fe_top
      ,.r_uncached_o(ptag_uncached_li)
      ,.r_nonidem_o(ptag_nonidem_li)
      ,.r_dram_o(ptag_dram_li)
-     ,.r_instr_misaligned_o(instr_misaligned_v)
+     ,.r_instr_misaligned_o()
      ,.r_load_misaligned_o()
      ,.r_store_misaligned_o()
      ,.r_instr_access_fault_o(instr_access_fault_v)
@@ -331,15 +331,15 @@ module bp_fe_top
      ,.stat_mem_o(stat_mem_o)
      );
 
-  logic itlb_miss_r, instr_misaligned_r, instr_access_fault_r, instr_page_fault_r;
+  logic itlb_miss_r, instr_access_fault_r, instr_page_fault_r;
   bsg_dff_reset
-   #(.width_p(4))
+   #(.width_p(3))
    fault_reg
     (.clk_i(clk_i)
      ,.reset_i(reset_i)
 
-     ,.data_i({ptag_miss_li, instr_misaligned_v, instr_access_fault_v, instr_page_fault_v})
-     ,.data_o({itlb_miss_r, instr_misaligned_r, instr_access_fault_r, instr_page_fault_r})
+     ,.data_i({ptag_miss_li, instr_access_fault_v, instr_page_fault_v})
+     ,.data_o({itlb_miss_r, instr_access_fault_r, instr_page_fault_r})
      );
 
   logic v_if1_r, v_if2_r;
@@ -357,7 +357,7 @@ module bp_fe_top
 
   wire icache_miss    = v_if2_r & ~icache_data_v_lo;
   wire queue_miss     = v_if2_r & ~fe_queue_ready_i;
-  wire fe_exception_v = v_if2_r & (instr_misaligned_r | instr_access_fault_r | instr_page_fault_r | itlb_miss_r | icache_miss_v_lo);
+  wire fe_exception_v = v_if2_r & (instr_access_fault_r | instr_page_fault_r | itlb_miss_r | icache_miss_v_lo);
   wire fe_instr_v     = v_if2_r & icache_data_v_lo;
   assign fe_queue_v_o = fe_queue_ready_i & (fe_instr_v | fe_exception_v) & ~cmd_nonattaboy_v;
 
@@ -381,13 +381,11 @@ module bp_fe_top
         fe_queue_cast_o.msg.exception.vaddr          = fetch_pc_lo;
         fe_queue_cast_o.msg.exception.exception_code = itlb_miss_r
                                                        ? e_itlb_miss
-                                                       : instr_misaligned_r
-                                                         ? e_instr_misaligned
-                                                           : instr_page_fault_r
-                                                             ? e_instr_page_fault
-                                                             : instr_access_fault_r
-                                                               ? e_instr_access_fault
-                                                                 : e_icache_miss;
+                                                         : instr_page_fault_r
+                                                           ? e_instr_page_fault
+                                                           : instr_access_fault_r
+                                                             ? e_instr_access_fault
+                                                               : e_icache_miss;
       end
     else
       begin

commit c23898f797bd62270379fb14d278d5810c73277a

Fix instruction page fault exception type
Bug introduced in #1100

bp_be/src/v/bp_be_checker/bp_be_scheduler.sv
@@ -211,7 +211,7 @@ module bp_be_scheduler
 
       dispatch_pkt.exception.instr_access_fault |=
         fe_exc_not_instr_li & fe_queue_lo.msg.exception.exception_code inside {e_instr_access_fault};
-      dispatch_pkt.exception.instr_misaligned   |=
+      dispatch_pkt.exception.instr_page_fault   |=
         fe_exc_not_instr_li & fe_queue_lo.msg.exception.exception_code inside {e_instr_page_fault};
       dispatch_pkt.exception.itlb_miss          |=
         fe_exc_not_instr_li & fe_queue_lo.msg.exception.exception_code inside {e_itlb_miss};